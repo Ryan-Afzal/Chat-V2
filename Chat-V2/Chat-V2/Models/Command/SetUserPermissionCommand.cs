9c74978719a4f64be99f22761188ff1120db7cfc
Ryan-Afzal <ryan.afzal@outlook.com>
2019-09-15 14:05:21 -0700

    Began work on SetUserPermissionCommand

---
+++ Chat-V2/Chat-V2/Models/Command/SetUserPermissionCommand.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chat_V2.Models.Command {
+	public class SetUserPermissionCommand : Command {
+
+		public SetUserPermissionCommand() : base(1, "set-permission", "Sets the permission of the specified user.", "set-permission <userId> <newRank>") { }
+
+		public async override Task Execute(CommandArgs args) {
+			throw new NotImplementedException();
+		}
+
+	}
+}
