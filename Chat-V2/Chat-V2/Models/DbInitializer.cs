bd3c0c9261f7a216107230afb045e0dfe7f2f9e2
Ryan-Afzal <ryan.afzal@outlook.com>
2019-09-14 21:14:33 -0700

    Made myself the admin

--- Chat-V2/Chat-V2/Models/DbInitializer.cs
+++ Chat-V2/Chat-V2/Models/DbInitializer.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Chat_V2.Models {
 
 	public static class DbInitializer {
 
 		public static void Initialize(ChatContext context) {
-
+			
 			context.Database.EnsureCreated();
 
 			if (context.Group.Any()) {
