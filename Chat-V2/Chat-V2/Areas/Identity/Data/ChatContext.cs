2e221020432347e147c88f3f8b468a294dc8b65d
Ryan-Afzal <ryan.afzal@outlook.com>
2019-10-08 14:59:02 -0700

    did a test

--- Chat-V2/Chat-V2/Areas/Identity/Data/ChatContext.cs
+++ Chat-V2/Chat-V2/Areas/Identity/Data/ChatContext.cs
@@ -33,7 +33,5 @@ namespace Chat_V2.Models {
 		public DbSet<Membership> Membership { get; set; }
 		public DbSet<ChatMessage> ChatMessage { get; set; }
 
-		public DbSet<Image> Image { get; set; }
-
 	}
 }
