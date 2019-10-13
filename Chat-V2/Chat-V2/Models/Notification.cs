b658ebec581be5417a62181244cdef4aa85a54a7
Ryan-Afzal <ryan.afzal@outlook.com>
2019-10-12 22:13:51 -0700

    Added base class for notifs

---
+++ Chat-V2/Chat-V2/Models/Notification.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chat_V2.Models {
+	public class Notification {
+		[Key]
+		public int ChatUserID { get; set; }
+		public string FormattedText { get; set; }
+	}
+}
