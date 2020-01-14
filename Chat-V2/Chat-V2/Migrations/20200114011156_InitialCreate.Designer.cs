c16d0c07caa690e5472b18c9f5b520d056c7f173
Ryan-Afzal <ryan.afzal@outlook.com>
2020-01-13 17:12:24 -0800

    Added another migration

--- Chat-V2/Chat-V2/Migrations/20200114011156_InitialCreate.Designer.cs
+++ Chat-V2/Chat-V2/Migrations/20200114011156_InitialCreate.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Chat_V2.Migrations
 {
     [DbContext(typeof(ChatContext))]
-    [Migration("20200113233236_InitialCreate")]
+    [Migration("20200114011156_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
